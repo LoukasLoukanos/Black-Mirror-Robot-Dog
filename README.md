7ae6cfaff1b79daede756e05f5bd4d6fd6832ffd
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-07-21 15:53:30 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Black Mirror Robot Dog
-- Crie seu cão robô assassino black mirror
+### Crie seu cão robô assassino da série black mirror
+Uma aplicação web simples cujo código JavaScript tem o objetivo de permitir a interação do usuário com os elementos da página para modificar as estatísticas do robô com as informações atualizadas na página em tempo real.
+
+
 - Imagens com direitos autorais: https://www.myminifactory.com/object/3d-print-black-mirror-metalhead-dog-65052
--- js/main.js
+++ js/main.js
@@ -6,40 +6,38 @@ const controle = document.querySelectorAll("[data-controle]")
 todos os elementos do documento que possuem o atributo "data-estatistica".*/
 const estatisticas = document.querySelectorAll("[data-estatistica]")
 
-/* Define um objeto chamado "pecas" que contém várias propriedades, onde cada propriedade 
-é uma peça do robô. Cada peça tem suas próprias propriedades, como "forca", "poder", 
-"energia" e "velocidade", com valores numéricos associados a elas.*/
-const pecas = {
-    "bracos": {
-        "forca": 29,
-        "poder": 35,
-        "energia": -21,
-        "velocidade": -5
+/* Define um objeto chamado "pontuacao_caracteristicas" que contém várias propriedades, onde cada propriedade 
+é uma caracteristica do robô.*/
+const pontuacao_caracteristicas = {
+    "intensidade": {
+        "forca": 13,
+        "poder": 9,
+        "energia": -7,
+        "velocidade": 5
     },
-
-    "blindagem": {
-        "forca": 41,
-        "poder": 20,
-        "energia": 0,
-        "velocidade": -20
-    },
-    "nucleos":{
+    "processamento": {
         "forca": 0,
+        "poder": 6,
+        "energia": -9,
+        "velocidade": 15
+    },
+    "blindagem":{
+        "forca": 12,
+        "poder": 15,
+        "energia": -8,
+        "velocidade": -11
+    },
+    "agilidade":{
+        "forca": 3,
+        "poder": 4,
+        "energia": -6,
+        "velocidade": 14
+    },
+    "resistencia":{
+        "forca": -4,
         "poder": 7,
-        "energia": 48,
-        "velocidade": -24
-    },
-    "pernas":{
-        "forca": 27,
-        "poder": 21,
-        "energia": -32,
-        "velocidade": 42
-    },
-    "foguetes":{
-        "forca": 0,
-        "poder": 28,
-        "energia": 0,
-        "velocidade": -2
+        "energia": 14,
+        "velocidade": 0
     }
 }
 
@@ -48,28 +46,37 @@ passando o valor do atributo "data-controle" do elemento clicado e o pai do elem
 passando o valor do atributo "data-peca" do elemento clicado. */
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-        atualizaEstatistica(evento.target.dataset.peca)
+        manipulaDados(evento, evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
 /* Seleciona o elemento dentro do "controle" que possui o atributo "data-contador" e armazena em uma constante chamada "peca". Em seguida, 
 verifica se a "operacao" é igual a "-", se for, subtrai 1 do valor de "peca.value", caso contrário, adiciona 1 ao valor de "peca.value". */
-function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("[data-contador]");
+function manipulaDados(evento, operacao, controle) {
+    const capacidade_total = controle.querySelector("[data-contador]");
 
     if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1;
-        atualizaEstatistica(evento.target.dataset.peca, -1); // Chamada da função com valor -1
+        capacidade_total.value = parseInt(capacidade_total.value) - 1;
+        atualizaEstatistica(evento.target.dataset.botao_operacao, -1); // Chamada da função com valor -1
     } else {
-        peca.value = parseInt(peca.value) + 1;
-        atualizaEstatistica(evento.target.dataset.peca, 1); // Chamada da função com valor 1
+        capacidade_total.value = parseInt(capacidade_total.value) + 1;
+        atualizaEstatistica(evento.target.dataset.botao_operacao, 1); // Chamada da função com valor 1
     }
 }
 
-/* Percorre todos os elementos da NodeList "estatisticas". Para cada elemento, atualiza o seu texto interno (textContent) somando o valor numérico correspondente à propriedade do objeto "pecas" relacionada à "peca" e à propriedade "data-estatistica" do elemento. */
-function atualizaEstatistica(peca, valor) {
+/* Percorre todos os elementos da NodeList "estatisticas". Para cada elemento, atualiza o seu texto interno (textContent) somando o valor numérico correspondente à propriedade do objeto "pontuacao_caracteristicas" relacionada à "peca" e à propriedade "data-estatistica" do elemento. */
+function atualizaEstatistica(botao_operacao, valor) {
     estatisticas.forEach((elemento) => {
-        elemento.textContent = parseInt(elemento.textContent) + (pecas[peca][elemento.dataset.estatistica] * valor);
+        elemento.textContent = parseInt(elemento.textContent) + (pontuacao_caracteristicas[botao_operacao][elemento.dataset.estatistica] * valor);
     });
 }
+
+//--- Ação 'Iniciar produção' ----------------------------------
+
+document.getElementById('producao').addEventListener('click', function () {
+    const imagem_original = document.getElementById('robotdog');
+    const imagem_substituta = "img/robot-finish.png";
+    
+    // Substituir a imagem original pela imagem substituta
+    imagem_original.src = imagem_substituta;
+});
